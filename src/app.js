12e8caa8aaed4654529acc6bbb5e71f73317a2a9
Jeremy <jeremyholder@protonmail.com>
2025-03-07 22:37:57 -0700

    inital app opens

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Interactive Video Player</h1>
+      <p>React is now working inside Electron!</p>
+    </div>
+  );
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
 
 let mainWindow;
 
-app.whenReady().then(() => {
+function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
-  mainWindow.loadURL(`file://${path.join(__dirname, 'build', 'index.html')}`);
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000'); // Load React Dev Server
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html')); // Load built React files
+  }
+}
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
   });
 });
 
